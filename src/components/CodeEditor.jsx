50a9f01178a0fcc4f4c56c39ebf0ec38767bfc38
Silas Getachew <silogecho@gmail.com>
2022-09-30 08:31:52 +0300

    feat: add python language to codemirror

--- src/components/CodeEditor.jsx
+++ src/components/CodeEditor.jsx
@@ -2,24 +2,25 @@ import CodeMirror from '@uiw/react-codemirror'
 import { dracula } from '@uiw/codemirror-theme-dracula'
 import { javascript } from '@codemirror/lang-javascript'
 import { python } from '@codemirror/lang-python'
+import React, { useState } from 'react'
 
-import React from 'react'
-
-const extensions = [javascript({ jsx: true })]
+const extensions = [javascript({ jsx: true }), python()]
 
 function CodeEditor() {
     const onChange = React.useCallback((value, viewUpdate) => {
         console.log('value:', value)
     }, [])
     return (
-        <CodeMirror
-            className="w-full"
-            value="console.log('This is Codemiror')"
-            height="100vh"
-            theme={dracula}
-            extensions={extensions}
-            onChange={onChange}
-        />
+        <div className="w-full">
+            <CodeMirror
+                className="w-full"
+                value="console.log('This is Codemiror')"
+                height="100vh"
+                theme={dracula}
+                extensions={extensions}
+                onChange={onChange}
+            />
+        </div>
     )
 }
 export default CodeEditor
